f813ad05e89574a51d870d016f3f2d3817ebc16d
miltonaguirre <aguirremilton0@gmail.com>
2025-04-29 18:30:11 -0300

    modificada cor de vitoria

--- racha_cuca3x3.py
+++ racha_cuca3x3.py
@@ -178,7 +178,7 @@ def animar_movimento(tela, tabuleiro, direcao, fonte, som_clique):
 
 def exibir_vitoria(tela, fonte):
     tela.fill(BRANCO)
-    texto = fonte.render("VOCÊ VENCEU!", True, VERDE)
+    texto = fonte.render("VOCÊ VENCEU!", True, CINZA)
     tela.blit(texto, texto.get_rect(center=(TAMANHO // 2, TAMANHO // 2)))
     pygame.display.update()
     pygame.time.delay(3000)
